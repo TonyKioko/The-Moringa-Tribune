1b80a136a0e2be2feeb9bad6d0f20bd4762076fd
TonyKioko <tonykioko384@gmail.com>
2018-09-24 15:02:27 +0300

    date format

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     url(r'^$',views.welcome,name = 'welcome'),
     url('^today/$',views.news_of_day,name='newsToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')
 ]
