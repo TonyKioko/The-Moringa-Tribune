bb790c766fa8d7b7cfff62b81a558eb868e96b7a
TonyKioko <tonykioko384@gmail.com>
2018-09-25 11:07:43 +0300

    edit views

--- news/views.py
+++ news/views.py
@@ -1,31 +1,47 @@
 from django.shortcuts import render, redirect
 from django.http  import HttpResponse,Http404
 from django.http  import HttpResponse
+from .models import Article
 import datetime as dt
 
 # Create your views here.
+
 def news_today(request):
-    return render(request, 'welcome.html')
-
-def news_of_day(request):
     date = dt.date.today()
-    return render(request, 'all-news/today-news.html', {"date": date,})
-# View Function to present news from past days
-def past_days_news(request, past_date):
+    news = Article.todays_news()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
 
+def past_days_news(request, past_date):
     try:
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
-
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
         assert False
 
     if date == dt.date.today():
-        return redirect(news_of_day)
+        return redirect(news_today)
 
-    return render(request, 'all-news/past-news.html', {"date": date})
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html',{"date": date,"news":news})
+
+# View Function to present news from past days
+# def past_days_news(request, past_date):
+#
+#     try:
+#         # Converts data from the string Url
+#         date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+#
+#     except ValueError:
+#         # Raise 404 error when ValueError is thrown
+#         raise Http404()
+#         assert False
+#
+#     if date == dt.date.today():
+#         return redirect(news_of_day)
+#
+#     return render(request, 'all-news/past-news.html', {"date": date})
 
     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
     # day = convert_dates(date)
